8b098a14ee445bdc27b95c6bed44f80688b67755
Alexandr707 <alexandralennikov90@gmail.com>
2022-10-26 14:11:30 +0700

    wpconf

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   devtool,
   entry: ["@babel/polyfill", path.resolve(__dirname, "src", "index.js")],
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     clean: true,
     filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/[name][ext]",
